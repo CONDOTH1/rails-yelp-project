61340d328438c78958da31657e3437bafeb2e146
Thady Condon <thadycondon@hotmail.com>
2016-08-24 14:37:44 +0100

    Fixed facebook sign in feature

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -26,8 +26,8 @@ Devise.setup do |config|
   # available as additional gems.
   require 'devise/orm/active_record'
   # config.omniauth :facebook, "FACEBOOK_APP_ID", "FACEBOOK_APP_SECRET",
-  config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret
-  p Rails.application.secrets.facebook_app_id
+  config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, scope: 'email', info_fields: 'email'
+  p "====================#{Rails.application.secrets.facebook_app_id}========================================"
   p Rails.application.secrets.facebook_app_secret
   # callback_url: "CALLBACK_URL"
   # ==> Configuration for any authentication mechanism
