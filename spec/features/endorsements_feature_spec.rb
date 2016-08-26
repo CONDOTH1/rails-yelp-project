781c5e0b5c322e8105d327e80da1238cc43a21b3
Thady Condon <thadycondon@hotmail.com>
2016-08-26 15:53:25 +0100

    Ajax functionality added and tests passing

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -7,13 +7,13 @@ feature 'endorsing reviews' do
     kfc.reviews.create(rating: 3, thoughts: 'It was an abomination', restaurant_id: kfc.id, user_id: user.id)
   end
 
-  scenario 'a user can endorse a review, which updates the review endorsement count' do
+  it 'a user can endorse a review, which increments the endorsement count', js: true do
     visit '/restaurants'
     click_link 'Endorse Review' #are we endorsing restaurants or the review of the restaurants?
     expect(page).to have_content('1 endorsement')
   end
 
-  scenario 'counts number of endorsements' do
+  it 'a user can endorse a review, which increments the endorsement count', js: true do
     visit '/restaurants'
     10.times{click_link 'Endorse Review'} #are we endorsing restaurants or the review of the restaurants?
     expect(page).to have_content('10 endorsements')
